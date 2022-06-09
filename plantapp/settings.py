b3d47a9be2b0030d20b8666f51b73fd4bb21b01a
Gugarauj07 <gugaraujo01@gmail.com>
2022-06-09 12:02:41 -0400

    all working

--- plantapp/settings.py
+++ plantapp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '34s66f2twc8c&q*i_@@4i(yp7wp0-d_hi3il7%1iom55xz(87w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
