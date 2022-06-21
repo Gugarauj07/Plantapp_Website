dfd4f4391651b3084b6a98fe03d36332990b2857
Gugarauj07 <gugaraujo01@gmail.com>
2022-06-21 16:37:51 -0400

    colors

--- plantapp/settings.py
+++ plantapp/settings.py
@@ -44,9 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'crispy_forms',
+    'crispy_forms',
 ]
-# CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 MIDDLEWARE = [
