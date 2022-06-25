cb50f3247aee133d427ded136e2b8c3b35f1bfbe
Gugarauj07 <gugaraujo01@gmail.com>
2022-06-25 19:32:34 -0400

    '.'

--- plantapp/settings.py
+++ plantapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '34s66f2twc8c&q*i_@@4i(yp7wp0-d_hi3il7%1iom55xz(87w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://inplantapp.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
